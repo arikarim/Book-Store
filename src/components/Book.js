dec6d1c3047c6bad43a4c77868ab8888c133fcd3
arikarim <arikarim146@gmail.com>
2021-06-01 22:37:29 +0300

    Implement book component

---
+++ src/components/Book.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+const Book = ({book}) => {
+  return (
+    <tr>
+      <td id={book.id}>{book.id}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+  </tr>
+  )
+}
+
+Book.propTypes = {
+
+}
+
+export default Book
