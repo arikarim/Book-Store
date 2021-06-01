9537f8e05644359fcbbc9858ed7e4645f278810d
arikarim <arikarim146@gmail.com>
2021-06-01 23:15:18 +0300

    Implement book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,10 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 
 export const addBook = (book) => ({
   type: CREATE_BOOK,
+  book: book
 });
 
 export const removeBook = (book) => ({
   type: REMOVE_BOOK,
+  book: book
 });
--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,5 +1,11 @@
-const bookReducer = (state = state, action) => {
+
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CREATE_BOOK':
+    case CREATE_BOOK:
+      // some actions
+    case REMOVE_BOOK:
+      // some actions
+    default:
+      return state
   }
 };
