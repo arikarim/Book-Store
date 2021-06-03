4529a3935ff6c2f3d6b93519ba141fa10d62677a
arikarim <arikarim146@gmail.com>
2021-06-03 12:37:57 +0300

    Add categoryFilter

---
+++ src/components/categoryFilter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const categoryFilter = ({filter}) => {
+  return (
+    // <select
+    //   value={filter}>
+    //   <option value="All">All</option>
+    //   { categories.map((c) => <option key={c} value={c}>{ c }</option>) }
+    // </select>
+  )
+}
+
+categoryFilter.propTypes = {
+
+}
+
+export default categoryFilter
